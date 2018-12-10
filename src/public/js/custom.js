e6099eea3b1a68bbc719631623085d8af10e924d
0ya-sh0 <ymthatte@gmail.com>
2018-12-10 18:27:22 +0530

    added processor options

---
+++ src/public/js/custom.js
@@ -0,0 +1,28 @@
+var processors;
+
+function loadAllProcessors() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            processors = JSON.parse(this.responseText)
+            createProcessorOptions(processors)
+        }
+    };
+    xhttp.open("GET", "/api/components/processors", true);
+    xhttp.send();
+}
+
+function createProcessorOptions(processors) {
+    var select = document.getElementById('processor-selector');
+    for(var i=0; i<processors.length; i++) {       
+        var option = document.createElement("option");
+        option.setAttribute("value", processors[i].p_model);
+        var model = document.createTextNode(processors[i].p_model);
+        option.appendChild(model);
+        select.appendChild(option);
+    }
+}
+
+window.onload = function() {
+    loadAllProcessors();
+};
