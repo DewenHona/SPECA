8cfd81db4ce21e6b7264003d12f45168769f452a
0ya-sh0 <ymthatte@gmail.com>
2018-12-10 22:02:21 +0530

    basic login form

---
+++ src/public/js/login.js
@@ -0,0 +1,15 @@
+function login() {
+    /*var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "/api/login", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify({
+        "username": `${document.getElementById('username').value}`, 
+        "password": `${document.getElementById('password').value}`
+    }));   
+    */
+}
