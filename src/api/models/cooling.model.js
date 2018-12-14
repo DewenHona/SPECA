0549a2e64d0626bf3d180c94c7a92ae5660e4930
0ya-sh0 <ymthatte@gmail.com>
2018-12-14 17:04:46 +0530

    cooling route

---
+++ src/api/models/cooling.model.js
@@ -0,0 +1,41 @@
+const sql = require('./db');
+
+/*
+    cooler_id int(100) NOT NULL AUTO_INCREMENT,
+ cooler_brand varchar(100) COLLATE utf8_unicode_ci NOT NULL, 
+ cooler_model varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+*/
+
+const Cooling = function(id, brand, model) {
+    this.id = id;
+    this.brand = brand;
+    this.model = model;
+}
+
+Cooling.getAllCooling = function(result) {
+    sql.query('Select * from cooling_solution', function(err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else{
+            console.log('tasks : ', res);  
+            result(null, res);
+        }
+    });
+}
+
+Cooling.getCoolingById = function(id, result) {
+    sql.query(`Select * from cooling_solution where cooler_id = ${id}`, function(err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else{
+            console.log('tasks : ', res);  
+            result(null, res);
+        }
+    });
+}
+
+module.exports = Cooling;
--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -8,12 +8,14 @@ module.exports = function(app) {
     const psu = require('./psu')
     const ram = require('./ram');
     const storage = require('./storage');
+    const cooling = require('./cooling');
     app.use('/api/components',component);
     app.use('/api/components/processors',processor);
     app.use('/api/components/motherboards',motherboard);
     app.use('/api/components/graphics',graphic);
     app.use('/api/components/ram',ram);
     app.use('/api/components/psu', psu);
+    app.use('/api/components/cooling', cooling);
     app.use('/api/components/storage', storage);
     app.use('/api/auth',auth);
 };
