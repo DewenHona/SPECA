e6154efe977512bd838d8d81df69554578888a9c
DewenHona <45680052+DewenHona@users.noreply.github.com>
2018-12-16 18:47:25 +0530

    debugged

--- src/public/js/question.js
+++ src/public/js/question.js
@@ -1,157 +1,159 @@
 const config = [
-        // Level 0 Question //
-        {
-            question: "Select your category",
-            options: [{
-                    option: "Editing",
-                    question: 1
-                },
-                {
-                    option: "Gaming",
-                    question: 2
-                },
+    // Level 0 Question //
+    {
+        question: "Select your category",
+        options: [{
+                option: "Editing",
+                question: 1
+            },
+            {
+                option: "Gaming",
+                question: 2
+            },
 
-                {
-                    option: "Development",
-                    question: 3
-                },
+            {
+                option: "Development",
+                question: 3
+            },
 
-                // {
-                //     option: "General"
-                //     question:
-                // }
-            ]
-        },
-
-
-        // Level 1 Question //        
-
-        {
-            question: "Select your type of Editing",
-            options: [{
-                    option: "Image",
-                    question: 4
-                },
-
-                {
-                    option: "Video",
-                    question: 4
-                },
-
-                {
-                    option: "Rendering",
-                    question: 4
-                }
-            ]
-        },
+            // {
+            //     option: "General"
+            //     question:
+            // }
+        ]
+    },
 
 
-        //Level 2 Question//
-        {
-            question: "Select your Games you plan to play",
-            options: [{
-                    option: "CS:GO",
-                    question: 4
-                },
-                {
-                    option: "PUBG",
-                    question: 4
-                },
+    // Level 1 Question //        
 
-                {
-                    option: "GTA V",
-                    question: 4
-                },
+    {
+        question: "Select your type of Editing",
+        options: [{
+                option: "Image",
+                question: 4
+            },
 
-                {
-                    option: "Dota 2",
-                    question: 4
-                }
-            ]
-        },
+            {
+                option: "Video",
+                question: 4
+            },
 
-        // Level 3 Question//
-        {
-            question: "Select your type of development",
-            options: [{
-                    option: "Web Development",
-                    question: 4
-                },
-                {
-                    option: "Desktop / Mobile Applications",
-                    question: 4
-                },
-
-                {
-                    option: "Articial Intelligence / Machine learning / Big Data",
-                    question: 4
-                },
-
-            ]
-        },
-
-        // Level 4 Question //
-        {
-            question: "Select Display Resolution",
-            options: [{
-                    option: "HD",
-                    question: 5
-                },
-                {
-                    option: "Full HD",
-                    question: 5
-                },
-
-                {
-                    option: "4K",
-                    question: 5
-                },
-            ]
-        },
-
-        // Level 5 Question //
-        {
-            question: "Do you want overclocking support ?",
-            options: [{
-                    option: "Yes",
-                    question: 6
-                },
-
-                {
-                    option: "No",
-                    question: 6
-                },
+            {
+                option: "Rendering",
+                question: 4
+            }
+        ]
+    },
 
 
-            ]
-        },
+    //Level 2 Question//
+    {
+        question: "Select your Games you plan to play",
+        options: [{
+                option: "CS:GO",
+                question: 4
+            },
+            {
+                option: "PUBG",
+                question: 4
+            },
 
-        // Level 6 Question //
-        {
-            question: "Do you plan to stream your content",
-            options: [{
-                    option: "Yes",
-                    question: 6
-                },
-                {
-                    option: "No",
-                    question: 6
-                },
-            ]
-        },
+            {
+                option: "GTA V",
+                question: 4
+            },
 
-        // Level 7 Question //
-        {
-            question: "What kind size of the system do you prefer ?",
-            options: [{
-                    option: "Normal",
-                    question: null
-                },
-                {
-                    option: "Compact",
-                    question: null
-                },
+            {
+                option: "Dota 2",
+                question: 4
+            }
+        ]
+    },
 
+    // Level 3 Question//
+    {
+        question: "Select your type of development",
+        options: [{
+                option: "Web Development",
+                question: 4
+            },
+            {
+                option: "Desktop / Mobile Applications",
+                question: 4
+            },
+
+            {
+                option: "Articial Intelligence / Machine learning / Big Data",
+                question: 4
             },
 
         ]
+    },
+
+    // Level 4 Question //
+    {
+        question: "Select Display Resolution",
+        options: [{
+                option: "HD",
+                question: 5
+            },
+            {
+                option: "Full HD",
+                question: 5
+            },
+
+            {
+                option: "4K",
+                question: 5
+            },
+        ]
+    },
+
+    // Level 5 Question //
+    {
+        question: "Do you want overclocking support ?",
+        options: [{
+                option: "Yes",
+                question: 6
+            },
+
+            {
+                option: "No",
+                question: 6
+            },
+
+
+        ]
+    },
+
+    // Level 6 Question //
+    {
+        question: "Do you plan to stream your content",
+        options: [{
+                option: "Yes",
+                question: 6
+            },
+            {
+                option: "No",
+                question: 6
+            },
+        ]
+    },
+
+    // Level 7 Question //
+    {
+        question: "What kind size of the system do you prefer ?",
+        options: [{
+                option: "Normal",
+                question: null
+            },
+            {
+                option: "Compact",
+                question: null
+            },
+
+        ]
+
+    }
+
+]
