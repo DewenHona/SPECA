2d21fd7d0cfc2aaebfda4f6e1b0513b4787b9701
DewenHona <45680052+DewenHona@users.noreply.github.com>
2018-12-18 18:04:40 +0530

    fixes

--- src/public/js/question.js
+++ src/public/js/question.js
@@ -1,200 +1,198 @@
 const configAuto = [
-    // Level 0 Question // 
-    {
-        question: "Select your category",
-        options: [{
-                option: "Editing.",
-                question: 1,
-                description: "This category is appropriate for people who do extensive work related the content creation which relies on various forms of media and focusing on the creative process. ",
+  // Level 0 Question //
+  {
+    question: "Select your category.",
+    options: [
+      {
+        option: "Editing.",
+        question: 1,
+        description:
+          "This category is appropriate for people who do extensive work related the content creation which relies on various forms of media and focusing on the creative process. "
+      },
+      {
+        option: "Gaming.",
+        question: 2,
+        description:
+          "If you are fond of playing vidoes games and want to no compromises while getting that sweet kill or a win, this category is the one you might be looking for. "
+      },
 
-            },
-            {
-                option: "Gaming.",
-                question: 2,
-                description: "If you are fond of playing vidoes games and want to no compromises while getting that sweet kill or a win, this category is the one you might be looking for. ",
+      {
+        option: "Development.",
+        question: 3,
+        description:
+          "If your work domain relies mainly on creation of the softwares, applications of any kind of development which relies on computations and logic, selection this category."
+      }
+    ]
+  },
 
-            },
+  // Level 1 Question //
 
-            {
-                option: "Development.",
-                question: 3,
-                description: "If your work domain relies mainly on creation of the softwares, applications of any kind of development which relies on computations and logic, selection this category."
+  {
+    question: "Select your type of Editing.",
+    options: [
+      {
+        option: "Image",
+        question: 4,
+        description:
+          "Select this option if your work relies editing graphics, photography work, or you main focus in on creating UI/UX mock ups. "
+      },
 
-            },
-        ]
-    },
+      {
+        option: "Video",
+        question: 4,
+        description:
+          "Select this option if you are avid flim maker, youtube creator or you create those smooth animations , tutorials or reviews. "
+      },
 
+      {
+        option: "Rendering",
+        question: 4,
+        description:
+          "Select this option if you deal with object assets, create video game enviornments, visual simulations and product desgining work."
+      }
+    ]
+  },
 
-    // Level 1 Question //        
+  //Level 2 Question//
+  {
+    question: "Select your Games you plan to play.",
+    options: [
+      {
+        option: " E-Sports - CS:GO / Dota 2 / Overwatch / Fortnite. ",
+        question: 4,
+        description:
+          "Select this option if you mostly play casual and competative games. "
+      },
+      {
+        option:
+          "AAA Titles - The Witcher / Batman / GTA V / Assassin's Creed / Crysis / PUBG.",
+        question: 4,
+        description:
+          "Select this option if you want to play most of the big games titles. "
+      },
 
-    {
-        question: "Select your type of Editing.",
-        options: [{
-                option: "Image",
-                question: 4,
-                description: "Select this option if your work relies editing graphics, photography work, or you main focus in on creating UI/UX mock ups. "
-            },
+      {
+        option: "Every kind of game. ",
+        question: 4,
+        description:
+          "Select this if your you want your system to run almost everything and anything. "
+      }
+    ]
+  },
 
-            {
-                option: "Video",
-                question: 4,
-                description: "Select this option if you are avid flim maker, youtube creator or you create those smooth animations , tutorials or reviews. ",
+  // Level 3 Question//
+  {
+    question: "Select your type of development.",
+    options: [
+      {
+        option: "Web Development.",
+        question: 4,
+        description:
+          "This option covers all the necessities to create and deploy web-based applications be it front-end, back-end or full stack development. "
+      },
+      {
+        option: "Desktop / Mobile Applications.",
+        question: 4,
+        description:
+          "Applications which rely on creating visual components driven by their logical operations and interactivity of their interfaces.  "
+      },
 
-            },
+      {
+        option: "Articial Intelligence / Machine learning / Big Data.",
+        question: 4,
+        description:
+          "If you want to create softwares which try to simulate intelligence and able to recognize patterns and produce theoritical output based on the data analyzed, select this option. "
+      }
+    ]
+  },
 
-            {
-                option: "Rendering",
-                question: 4,
-                description: "Select this option if you deal with object assets, create video game enviornments, visual simulations and product desgining work."
+  // Level 4 Question //
+  {
+    question: "Select Display Resolution.",
+    options: [
+      {
+        option: "Full HD (1080p).",
+        question: 5,
+        description:
+          "This option is the standard resolution and is appropriate for majority of work. Select this option if it satisties that."
+      },
+      {
+        option: "Wide Quard HD (2K / 1440p).",
+        question: 5,
+        description:
+          "Are you looking for something which can offer more than the standard and does lean too much towrd lard size this might the one you looking for"
+      },
 
-            }
-        ]
-    },
+      {
+        option: "Ultra HD (4K / 2160p).",
+        question: 5,
+        description:
+          "If you want the latest and greatest in terms of what displays offer look none other than this option."
+      }
+    ]
+  },
 
+  // Level 5 Question //
+  {
+    question: "Do you want overclocking support ?",
+    options: [
+      {
+        option: "Skip",
+        question: 6,
+        description:
+          "Overclocking refers to the manual action of increase in clock speed of computer componenets. Select skip if you do not know what this is about or simply don't plan to do it. "
+      },
 
-    //Level 2 Question//
-    {
-        question: "Select your Games you plan to play.",
-        options: [{
-                option: "CS:GO",
-                question: 4,
-                description: "Select the graphically simmilar and genres of games you are considering to play on the machine. "
-            },
-            {
-                option: "PUBG",
-                question: 4,
-                description: "Select the graphically simmilar and genres of games you are considering to play on the machine. "
-            },
+      {
+        option: "Moderate",
+        question: 6,
+        description:
+          "Select this if want light touch improvements over your system and plan to give a slight edge over the stock speeds"
+      },
 
-            {
-                option: "GTA V",
-                question: 4,
-                description: "Select the graphically simmilar and genres of games you are considering to play on the machine. "
-            },
+      {
+        option: "Extreme",
+        question: 6,
+        description:
+          "Select this option if you want to lean towards maximizing the unlocked potential of you componenet speeds. "
+      }
+    ]
+  },
 
-            {
-                option: "Dota 2",
-                question: 4,
-                description: "Select the graphically simmilar and genres of games you are considering to play on the machine. "
-            }
-        ]
-    },
+  // Level 6 Question //
+  {
+    question: "Do you plan to stream your content",
+    options: [
+      {
+        option: "Yes",
+        question: 6,
+        description:
+          "If you plan constant upload of your content on live streaming platforms select this options. "
+      },
+      {
+        option: "No",
+        question: 6,
+        description:
+          "Select no if you do not plan any kind of content streaming"
+      }
+    ]
+  },
 
-    // Level 3 Question//
-    {
-        question: "Select your type of development",
-        options: [{
-                option: "Web Development",
-                question: 4,
-                description: "This option covers all the necessities to create and deploy web-based applications be it front-end, back-end or full stack development. ",
-
-            },
-            {
-                option: "Desktop / Mobile Applications",
-                question: 4,
-                description: "Applications which rely on creating visual components driven by their logical operations and interactivity of their interfaces.  ",
-
-            },
-
-            {
-                option: "Articial Intelligence / Machine learning / Big Data",
-                question: 4,
-                description: "If you want to create softwares which try to simulate intelligence and able to recognize patterns and produce theoritical output based on the data analyzed, select this option. "
-
-            },
-
-        ]
-    },
-
-    // Level 4 Question //
-    {
-        question: "Select Display Resolution",
-        options: [{
-                option: "Full HD (1080p)",
-                question: 5,
-                description: "This option is the standard resolution and is appropriate for majority of work. Select this option if it satisties that.",
-
-            },
-            {
-                option: "Wide Quard HD (2K / 1440p)",
-                question: 5,
-                description: "Are you looking for something which can offer more than the standard and does lean too much towrd lard size this might the one you looking for",
-
-            },
-
-            {
-                option: "Ultra HD (4K / 2160p)",
-                question: 5,
-                description: "If you want the latest and greatest in terms of what displays offer look none other than this option., "
-
-            },
-        ]
-    },
-
-    // Level 5 Question //
-    {
-        question: "Do you want overclocking support ?",
-        options: [{
-                option: "Skip",
-                question: 6,
-                description: "Overclocking refers to the manual action of increase in clock speed of computer componenets. Select skip if you do not know what this is about or simply don't plan to do it. ",
-
-            },
-
-            {
-                option: "Moderate",
-                question: 6,
-                description: "Select this if want light touch improvements over your system and plan to give a slight edge over the stock speeds",
-
-            },
-
-            {
-                option: "Extreme",
-                question: 6,
-                description: "Select this option if you want to lean towards maximizing the unlocked potential of you componenet speeds. "
-
-            },
-
-
-        ]
-    },
-
-    // Level 6 Question //
-    {
-        question: "Do you plan to stream your content",
-        options: [{
-                option: "Yes",
-                question: 6,
-                description: "If you plan constant upload of your content on live streaming platforms select this options. ",
-
-            },
-            {
-                option: "No",
-                question: 6,
-                description: "Select no if you do not plan any kind of content streaming"
-
-            },
-        ]
-    },
-
-    // Level 7 Question //
-    {
-        question: "What kind size of the system do you prefer ?",
-        options: [{
-                option: "Normal",
-                question: null,
-                description: "Select this option if you want normal sized system. ",
-
-            },
-            {
-                option: "Compact",
-                question: null,
-                description: "Select this option if you want small sized and compact system. Keep in mind this may limit some additonal customizations"
-
-            },
-
-        ]
-
-    }
-
-]
+  // Level 7 Question //
+  {
+    question: "What kind size of the system do you prefer ?",
+    options: [
+      {
+        option: "Normal",
+        question: null,
+        description: "Select this option if you want normal sized system. "
+      },
+      {
+        option: "Compact",
+        question: null,
+        description:
+          "Select this option if you want small sized and compact system. Keep in mind this may limit some additonal customizations"
+      }
+    ]
+  }
+];
