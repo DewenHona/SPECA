aa5274b7b2385f992ef5a8ae71d92610c86b95ce
DewenHona <45680052+DewenHona@users.noreply.github.com>
2019-01-03 23:04:02 +0530

    change template

---
+++ src/public/js/description.js
@@ -0,0 +1,11 @@
+// Above the card, a question "Why is build is appropriate for you" will be always displayed.
+{
+    "Gaming_ES": {
+
+        "description": "Most games require a multi-threaded workload, which means a processor with multiple cores. ";
+
+    },
+    {
+        "Gaming_AA": " "
+    }
+}
