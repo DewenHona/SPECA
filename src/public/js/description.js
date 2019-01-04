140400dd5191328e844762250fb28029879f7d3d
DewenHona <45680052+DewenHona@users.noreply.github.com>
2019-01-04 22:00:14 +0530

    upto edit with wip particle js

--- src/public/js/description.js
+++ src/public/js/description.js
@@ -1,39 +1,121 @@
-// Above the card, a question "Why is build is appropriate for you" will be always displayed.
+// Above the this, a question "Why is build is appropriate for you" will be always displayed.
 [{
         "Editing_Vid": {
 
-            "description": " "
+            "description": [
+
+                "CPU: When it comes to editing for any kind the most importatn component in the whole build is the processor. Video editing require the power of the high clock rates or the processr and more cores will benefit render times and overall workflow of the project. Overclocking or not based on your choice approriate processor has been suggesed to you.",
+
+                "MOBO: While many may claim the motherboard may not be that important component, its still ties all the components together and is important to match correctly with other components. With respect to your editing, this motherboard is the one you are looking for. Proper considerations like your usage and how many slots you might require for components, this motherboard will satsify all you needs.",
+
+                "RAM: Memory capcaity is is not the only thing one must consider for an edting system. Proper type, speed and model are important considerations for an editing system and the one we recommened satisfies all",
+
+                "GPU: Most people are question as to how having a grpahics card will benefit an edting system. Well here is a news for you, most softwares used for video editing benefit from gpu cores at a considerable amount. Plus one cannot go wrong with having a graphic card in system. However a powerful one is not required but just about enough to get the jon done. The graphic for video edting has been sugguested consider everything. ",
+
+                "PSU: Power supply is the component which may people choose to ignore in terms of quality and priotorization, but ultimately its the component providing the means ofr energy to operate them. Ofcourse, considering all other components of the build a good power supply has already been determined.",
+
+                "SSD: Solid state drives are expensive components but for editing it is game changing addition. For most editing applications  rendering a big video file is quite hard for a tradtional storage device. A good combination of capacity and type has been sugguested, but it wont stop you to choose capcaity and type.",
+
+                "HHD:But then again, one cannot go wrong with the good old hard drives if you just need to store lot of data without worrying too much about speed. While we try to recommend based on average usage, its completely upto you to choose the right capacity.",
+
+                "CASE: The case is where you can really have fun selecting color and design, although of course there are practical considerations to overcome first. You need to select a case size that will fit and be compatible with your other components. Most of the components have slots that they need to fit in as well, so your PC case will need to provide enough of these for your components to fit inside with room for air flow. We have already determined best one for you.",
+
+                "COOLING: Technically a computer build is an electronic device, so yes it will require a good cooling especailly processor. Based on you requirements and options selected approriate solution has been stated.",
+
+                "DISPLAY: What resolution? What Refresh rate? Does it has g-sync support? What panel? All these question are major considerations while selecting a monitor but our system really knows which monitor is best for your editing needs.",
+            ]
 
         },
 
         "Editing_Ren": {
 
-            "description": " "
+            "description": [
 
+                "CPU: When it comes to editing for any kind the most important component in the whole build is the processor. Rendering requires the power of the high clock rates or the processor and more cores will benefit render times and overall workflow of the project. Overclocking or not based on your choice appropriate processor has been suggesed to you.",
+
+                "MOBO: While many may claim the motherboard may not be that important component, its still ties all the components together and is important to match correctly with other components. With respect to your editing, this motherboard is the one you are looking for. Proper considerations like your usage and how many slots you might require for components, this motherboard will satsify all you needs.",
+
+                "RAM: Memory capcaity is is not the only thing one must consider for an rendering objects. Proper type, speed and model are important considerations for a rendering system and the one we recommened satisfies all. ",
+
+                "GPU: Most people are question as to how having a grpahics card will benefit an rendering  system. Well here is a news for you, most softwares used for rendering benefit from gpu cores at a considerable amount. Plus one cannot go wrong with having a graphic card in system. However a powerful one is not required but just about enough to get the job done.The graphic for video edting has been sugguested consider everything.",
+
+                "PSU: Power supply is the component which may people choose to ignore in terms of quality and priotorization, but ultimately its the component providing the means of energy to operate them. Ofcourse, considering all other components of the build a good power supply has already been determined.",
+
+                "SSD: Solid state drives are expensive components but for rendering it is game changing addition. For most rendering applications rendering a big enviornments are quite hard for a tradtional storage device. A good combination of capacity and type has been sugguested, but it wont stop you to choose capcaity and type.",
+
+                "HHD:But then again, one cannot go wrong with the good old hard drives if you just need to store lot of data without worrying too much about speed. While we try to recommend based on average usage, its completely upto you to choose the right capacity.",
+
+                "COOLING: Technically a computer build is an electronic device, so yes it will require a good cooling especailly processor. Based on you requirements and options selected approriate solution has been stated.",
+
+                "DISPLAY: What resolution? What Refresh rate? Does it has g-sync support? What panel? All these question are major considerations while selecting a monitor but our system really knows which monitor is best for your editing needs.",
+
+            ]
         },
         "Gaming_ES": {
 
-            "description": " "
+            "description": [
+
+                "CPU: In gaming, a processor is not exactly the most important component in a system, but atleast a quard-core processor is required so thatother components will  not be bottlenecked by the it. Overclocking can be beneficial especially you plan to edit content along side gaming. AMD or Intel both are good brands, its all about requirement from then. ",
+
+                "MOBO: While many may claim the motherboard may not be that important component, its still ties all the components together and is important to matchcorrectly with other components. With respect to your choice, this motherboard is the one you are looking for.",
+
+                "RAM: Most people have big concerns with memory, however when it comes to any kind of modern gaming the standard ram capacity has been stated and approriate model have been suggested to you.",
+
+                "GPU:Graphics card is the most important component when it comes to a gaming computer, a good graphics card will allow the games to run smoothlyat that approriate resolution. There are so many to choose from, but out system has determined the best one for you E sports gaming require a moderate graphics card without leaning too much towards high end.",
+
+                "PSU: Power supply is the component which may people choose to ignore in terms of quality and priotorization, but ultimately its the component providing the means ofr energy to operate them. Ofcourse, considering all other components of the build a good power supply has already been determined.",
+
+                "SSD: Solid state drives are expensive components but for modern gaming, its important for faster load times and will save lot of time required for most applications, as esports are all about speed and accuracy, SSD is a must, well atleast for operating system drive.They are With greater lifespan than any other storage media and you wont even hear them running.A good combination of capacity and type has been suggested, but it wont stop you to choose capcaity and type.",
+
+                "HHD: But then again, one cannot go wrong with the good old hard drives if you just need to store lot of data without worrying too much about speed. While we try to recommend based on average usage, its completely upto you to choose the right capacity.",
+
+                "CASE: The case is where you can really have fun selecting color and design, although of course there are practical considerations to overcome first. You need to select a case size that will fit and be compatible with your other components. Most of the components have slots that they need to fit in as well, so your PC case will need to provide enough of these for your components to fit inside with room for air flow. We have already determined best one for you. ",
+
+                "COOLING: Technically a computer build is an electronic device, so yes it will require a good cooling especailly processor. Based on you requirements and options selected approriate solution has been stated.",
+
+                "DISPLAY: What resolution? What Refresh rate? Does it has g-sync support? All these question aare major considerations while selecting a monitor but our system really knows which monitor is best for for you needs."
+            ]
 
         },
         "Gaming_EA": {
 
-            "description": " "
+            "description": [
+
+                "CPU: In gaming, a processor is not exactly the most important component in a system, but atleast a quard-core processor is required so thatother components will  not be bottlenecked by the it. Overclocking can be beneficial especially you plan to edit content along side gaming. AMD or Intel both are good brands, its all about requirement from then. ",
+
+                "MOBO: While many may claim the motherboard may not be that important component, its still ties all the components together and is important to matchcorrectly with other components. With respect to your choice, this motherboard is the one you are looking for.",
+
+                "RAM: Most people have big concerns with memory, however when it comes to any kind of modern gaming the standard ram capacity has been stated and approriate model have been suggested to you.",
+
+                "GPU: Graphics card is the most important component when it comes to a gaming computer, a good graphics card will allow the games to run smoothly at that approriate resolution. There are so many to choose from, but our system has determined the best one for you AAA gaming require a capable graphics card with no comprmises and it is the one we have suggesed. ",
+
+                "PSU: Power supply is the component which may people choose to ignore in terms of quality and priotorization, but ultimately its the component providing the means ofr energy to operate them. Ofcourse, considering all other components of the build a good power supply has already been determined.",
+
+                "SSD:Solid state drives are expensive components but for modern gaming, its important for faster load times and will save lot of time required for most applications, as AAA Titles are all about heavy visuals ,hence SSD is a must, well atleast for operating system drive. They are With greater lifespan than any other storage media and you wont even hear them running. A good combination for capacity and type has been sugguested, but it wont stop you to choose capacity and type.",
+
+                "HHD: But then again, one cannot go wrong with the good old hard drives if you just need to store lot of data without worrying too much about speed. While we try to recommend based on average usage, its completely upto you to choose the right capacity.",
+
+                "CASE: The case is where you can really have fun selecting color and design, although of course there are practical considerations to overcome first. You need to select a case size that will fit and be compatible with your other components. Most of the components have slots that they need to fit in as well, so your PC case will need to provide enough of these for your components to fit inside with room for air flow. We have already determined best one for you. ",
+
+                "COOLING: Technically a computer build is an electronic device, so yes it will require a good cooling especailly processor. Based on you requirements and options selected approriate solution has been stated.",
+
+                "DISPLAY: What resolution? What Refresh rate? Does it has g-sync support? All these question aare major considerations while selecting a monitor but our system really knows which monitor is best for for you needs."
+            ]
 
         },
         "Development_Web": {
 
-            "description": " "
+            "description": "  to be added"
 
         },
         "Development_App": {
 
-            "description": " "
+            "description": " to be added "
 
         },
         "Development_AI": {
 
-            "description": " "
+            "description": " to be added"
 
         },
 
