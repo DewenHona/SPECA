ae91a4b7530c1eb1135ab7157a6fadeca3b97f92
0ya-sh0 <ymthatte@gmail.com>
2019-01-06 13:50:50 +0530

    add bkmrk

---
+++ src/public/js/allpart.js
@@ -0,0 +1,66 @@
+window.onload = () => {
+    if (sessionStorage.token) load();
+}
+
+var Components = {
+
+}
+
+var config = {
+    processors: [1,'p_id', ['p_model']],
+    motherboards: [2,'m_id', ['m_name']],
+    ram: [3,'r_id', ['r_brand', 'r_model', 'r_speed', 'r_capacity']],
+    graphics: [4,'g_id', ['g_model', 'g_vram']],
+    ssd: [5,'s_id', ['s_type', 's_brand', 's_model', 's_capacity']],
+    hdd: [6,'s_id', ['s_type', 's_brand', 's_model', 's_capacity']],
+    psu: [7,'psu_id', ['psu_brand', 'psu_model', 'psu_rating', 'psu_modular']],
+    case: [8,'c_id', ['c_brand', 'c_model', 'c_form_factor']],
+    cooling: [9,'cooler_id', ['cooler_brand', 'cooler_model']],
+    display: [10,'disp_id', ['disp_resolution', 'disp_refresh_rate', 'disp_size_type', 'disp_panel_type']]
+}
+
+function load() {
+    for (key in config)
+        loadComponent(key);
+}
+
+function loadComponent(apiname) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            Components[apiname] = JSON.parse(this.responseText);
+            displayComponent(apiname)
+        }
+    };
+    xhttp.open("GET", "/api/components/" + apiname, true);
+    xhttp.send();
+}
+
+function displayComponent(apiname) {
+    console.log(apiname)
+    var parentDiv = document.getElementById(apiname+"-bookmark");
+    var comp = Components[apiname]
+    var attr = config[apiname]
+    for(var i=0; i< comp.length; i++) {
+        var childDiv = document.createElement('div');
+        childDiv.setAttribute('class', 'part');
+        var label = document.createElement('label');
+        label.setAttribute('class', 'part-name');
+        var name = ''
+        attr[2].forEach(element => {
+            name += comp[i][element] + " "
+        });
+        label.innerHTML = name;
+        childDiv.appendChild(label);
+        var btn = document.createElement('button');
+        btn.setAttribute('class', 'save');
+        btn.innerHTML = "Save";
+        const type = attr[0];
+        const id = Components[apiname][i][attr[1]];
+        btn.onclick = function() {
+            postBookmark(type,id)
+        }
+        childDiv.appendChild(btn);
+        parentDiv.appendChild(childDiv);
+    }
+}
