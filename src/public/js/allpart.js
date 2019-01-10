f82d5e92d52145cda77159bb087b262c95ea14c1
DewenHona <45680052+DewenHona@users.noreply.github.com>
2019-01-10 21:17:45 +0530

    sss

--- src/public/js/allpart.js
+++ src/public/js/allpart.js
@@ -7,16 +7,16 @@ var Components = {
 }
 
 var config = {
-    processors: [1,'p_id', ['p_model']],
-    motherboards: [2,'m_id', ['m_name']],
-    ram: [3,'r_id', ['r_brand', 'r_model', 'r_speed', 'r_capacity']],
-    graphics: [4,'g_id', ['g_model', 'g_vram']],
-    ssd: [5,'s_id', ['s_type', 's_brand', 's_model', 's_capacity']],
-    hdd: [6,'s_id', ['s_type', 's_brand', 's_model', 's_capacity']],
-    psu: [7,'psu_id', ['psu_brand', 'psu_model', 'psu_rating', 'psu_modular']],
-    case: [8,'c_id', ['c_brand', 'c_model', 'c_form_factor']],
-    cooling: [9,'cooler_id', ['cooler_brand', 'cooler_model']],
-    display: [10,'disp_id', ['disp_resolution', 'disp_refresh_rate', 'disp_size_type', 'disp_panel_type']]
+    processors: [1, 'p_id', ['p_model']],
+    motherboards: [2, 'm_id', ['m_name']],
+    ram: [3, 'r_id', ['r_brand', 'r_model', 'r_speed', 'r_capacity']],
+    graphics: [4, 'g_id', ['g_model', 'g_vram']],
+    ssd: [5, 's_id', ['s_type', 's_brand', 's_model', 's_capacity']],
+    hdd: [6, 's_id', ['s_type', 's_brand', 's_model', 's_capacity']],
+    psu: [7, 'psu_id', ['psu_brand', 'psu_model', 'psu_rating', 'psu_modular']],
+    case: [8, 'c_id', ['c_brand', 'c_model', 'c_form_factor']],
+    cooling: [9, 'cooler_id', ['cooler_brand', 'cooler_model']],
+    display: [10, 'disp_id', ['disp_resolution', 'disp_refresh_rate', 'disp_size_type', 'disp_panel_type']]
 }
 
 function load() {
@@ -38,10 +38,10 @@ function loadComponent(apiname) {
 
 function displayComponent(apiname) {
     console.log(apiname)
-    var parentDiv = document.getElementById(apiname+"-bookmark");
+    var parentDiv = document.getElementById(apiname + "-bookmark");
     var comp = Components[apiname]
     var attr = config[apiname]
-    for(var i=0; i< comp.length; i++) {
+    for (var i = 0; i < comp.length; i++) {
         var childDiv = document.createElement('div');
         childDiv.setAttribute('class', 'part');
         var img = document.createElement('img');
@@ -49,8 +49,8 @@ function displayComponent(apiname) {
         const id = Components[apiname][i][attr[1]];
         const src = `/images/components/${type}/${id}.png`
         img.setAttribute('src', src);
-        img.setAttribute('height', '120px');
-        img.setAttribute('width', '120px');
+        img.setAttribute('height', '170px');
+        img.setAttribute('width', '170px');
         childDiv.appendChild(img);
         var label = document.createElement('label');
         label.setAttribute('class', 'part-name');
@@ -63,8 +63,8 @@ function displayComponent(apiname) {
         var btn = document.createElement('button');
         btn.setAttribute('class', 'save');
         btn.innerHTML = "Save";
-        btn.onclick = function() {
-            postBookmark(type,id)
+        btn.onclick = function () {
+            postBookmark(type, id)
         }
         childDiv.appendChild(btn);
         parentDiv.appendChild(childDiv);
